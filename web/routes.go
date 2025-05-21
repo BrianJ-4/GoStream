2214b19b504b96fe8c2dc8d8d89d9803a2e9f54f
BrianJ-4 <brianjdev4@gmail.com>
2025-05-21 15:11:48 -0700

    Update logging

--- web/routes.go
+++ web/routes.go
@@ -8,13 +8,13 @@ import (
 )
 
 func home(w http.ResponseWriter, req *http.Request) {
-	log.Printf("GET /")
+	log.Printf("Incoming from %s: GET /", req.RemoteAddr)
 	w.Write([]byte("HOME"))
 }
 
 func video(w http.ResponseWriter, req *http.Request) {
 	fileName := req.PathValue("fileName")
-	log.Printf("GET /videos/%s", fileName)
+	log.Printf("Incoming from %s: GET /videos/%s %s", req.RemoteAddr, fileName, req.Header.Get("Range"))
 
 	// Check if requested video exists
 	err := file.CheckFileExists(fileName)
@@ -26,13 +26,13 @@ func video(w http.ResponseWriter, req *http.Request) {
 
 	// Handle intial probe
 	if req.Header.Get("Range") == "" {
-		err := handleInitialProbe(w, fileName)
+		err := handleInitialProbe(w, fileName, req.RemoteAddr)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 	} else { // Handle range request
-		err := handleRangeRequest(w, req.Header.Get("Range"), fileName)
+		err := handleRangeRequest(w, req.Header.Get("Range"), fileName, req.RemoteAddr)
 		if err != nil {
 			return
 		}
--- web/streaming.go
+++ web/streaming.go
@@ -23,7 +23,7 @@ type Range struct {
 
 const chunkSize = int64(5 * 1024 * 1024) // 5mb
 
-func handleInitialProbe(w http.ResponseWriter, fileName string) error {
+func handleInitialProbe(w http.ResponseWriter, fileName string, reqAddr string) error {
 	// Open Video
 	video, err := file.OpenFile(fileName)
 	if err != nil {
@@ -54,10 +54,11 @@ func handleInitialProbe(w http.ResponseWriter, fileName string) error {
 	w.Header().Set("Accept-Ranges", "bytes")
 
 	w.WriteHeader(http.StatusOK)
+	log.Printf("Outgoing to %s: %s", reqAddr, w.Header())
 	return nil
 }
 
-func handleRangeRequest(w http.ResponseWriter, requestRange string, fileName string) error {
+func handleRangeRequest(w http.ResponseWriter, requestRange string, fileName string, reqAddr string) error {
 	// Open Video
 	video, err := file.OpenFile(fileName)
 	if err != nil {
@@ -99,7 +100,7 @@ func handleRangeRequest(w http.ResponseWriter, requestRange string, fileName str
 		log.Print("Error getting data from video: ", err)
 		return err
 	}
-
+	log.Printf("Outgoing to %s: %s Start: %s", reqAddr, w.Header(), strconv.FormatInt(r.Start, 10))
 	return nil
 }
 
