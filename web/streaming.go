78ef51382a37afd846af837b8e3ff5e19a552582
BrianJ-4 <brianjdev4@gmail.com>
2025-05-21 04:00:54 -0700

    Began implementation of handleRangeRequest and parseRange

--- web/streaming.go
+++ web/streaming.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/BrianJ-4/GoStream/file"
 )
@@ -14,6 +16,8 @@ var extensionMapping = map[string]string{
 	".webm": "webm",
 	".ogg":  "ogg",
 }
+
+const chunkSize = int64(1024 * 1024) // 1mb
 
 func handleInitialProbe(w http.ResponseWriter, fileName string) error {
 	// Open Video
@@ -37,7 +41,7 @@ func handleInitialProbe(w http.ResponseWriter, fileName string) error {
 	// Set Content-Length
 	size, err := file.GetFileSize(video)
 	if err != nil {
-		log.Print("Error getting video length: ", err)
+		log.Print("Error getting video size: ", err)
 		return err
 	}
 	w.Header().Set("Content-Length", strconv.FormatInt(size, 10))
@@ -47,4 +51,82 @@ func handleInitialProbe(w http.ResponseWriter, fileName string) error {
 
 	w.WriteHeader(http.StatusOK)
 	return nil
+}
+
+func handleRangeRequest(w http.ResponseWriter, requestRange string, fileName string) error {
+	// Open Video
+	video, err := file.OpenFile(fileName)
+	if err != nil {
+		log.Print("Error opening file: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	}
+	defer video.Close()
+
+	// Get video size
+	size, err := file.GetFileSize(video)
+	if err != nil {
+		log.Print("Error getting video size: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	}
+
+	r, err := parseRange(requestRange, size)
+	if err != nil {
+		w.WriteHeader(http.StatusRequestedRangeNotSatisfiable)
+		return err
+	}
+	if 0 > 1 {
+		fmt.Print(r)
+	}
+
+	// Add Accept-Ranges header
+	w.Header().Set("Accept-Ranges", "bytes")
+
+	w.WriteHeader(http.StatusPartialContent)
+	return nil
+}
+
+type Range struct {
+	Start  int64
+	Length int64
+}
+
+func parseRange(requestRange string, size int64) (Range, error) {
+	// Range examples: bytes=0-; bytes=524288-; bytes=500-999; bytes=-100
+	var r Range
+
+	// Not supporting multipart ranges
+	if strings.Contains(requestRange, ",") {
+		err := errors.New("multipart ranges not supported")
+		log.Print("Error parsing range: ", err)
+		return r, err
+	}
+
+	requestRange = strings.Trim(requestRange, "bytes=") // Strips "bytes="
+	parts := strings.Split(requestRange, "-")
+
+	if len(parts) != 2 {
+		err := errors.New("invalid range format")
+		log.Print("Error parsing range: ", err)
+		return r, err
+	}
+	// Suffix range
+	if parts[0] == "" {
+		length, err := strconv.ParseInt(parts[1], 10, 64)
+		if err != nil {
+			log.Print("Error parsing suffix range: ", err)
+			return r, err
+		}
+		if length > size {
+			length = size
+		}
+		r.Start = size - length
+		r.Length = length
+	}
+
+	if r.Length > chunkSize {
+		r.Length = chunkSize
+	}
+	return r, nil
 }
